7f3c9628acb3d9fb60d1fff6d51df5c402c036b9
Larry Nickerson <lnickers@austin.rr.com>
2015-04-26 20:50:38 -0500

    comitting to my lnickers branch

--- ch2-getting-started-with-node/math.js
+++ ch2-getting-started-with-node/math.js
@@ -1,3 +1,6 @@
 /**
  * Created by larrynickerson on 4/26/15.
  */
+
+//lnickers2004 comment added as pull request test from a personal branch
+// i made a branch called lnickers2004branch and added this comment
