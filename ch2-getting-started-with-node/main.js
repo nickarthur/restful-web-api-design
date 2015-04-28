c3e6574092fd6431b72967d0cb6c27045b000342
Larry Nickerson <lnickers@austin.rr.com>
2015-04-27 22:52:31 -0500

    created module and added code to main file to use module

--- ch2-getting-started-with-node/main.js
+++ ch2-getting-started-with-node/main.js
@@ -1,1 +1,7 @@
+var httpModule = require('./modules/http-module');
+var http = require('http');
+var port = 8180;
 
+http.createServer(httpModule.handle_request).listen(port, '127.0.0.1');
+
+console.log('Started node.js http server at http://127.0.0.1:' + port);
